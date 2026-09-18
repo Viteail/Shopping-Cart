568566d007c7f3773d4d74942b097c2766331655
Viteail <vitea.sirbu1@gmail.com>
2026-09-18 16:17:29 +0300

    add alias paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,18 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@styles": path.resolve(import.meta.dirname, "src/styles"),
+      "@images": path.resolve(import.meta.dirname, "./src/assets"),
+      "@components": path.resolve(import.meta.dirname, "./src/components/"),
+    },
+  },
   test: {
     environment: "jsdom",
     setupFiles: "./src/testing/setup.ts",
